4efa6b1e6da9880c1c5208c81b141382532c3a22
Sascha Möllering <sascha.moellering@zanox.com>
2014-12-28 22:04:32 +0100

    Initial commit

---
+++ service/docker-registry.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# chkconfig: 2345 95 05
+# description: docker-registry
+#
+
+export SETTINGS_FLAVOR=prod
+BASE=$(basename $0)
+DOCKER_REGISTRY_PIDFILE=/var/run/$BASE.pid
+
+fail_unless_root() {
+  if [ "$(id -u)" != '0' ]; then
+    log_failure_msg "docker-registry must be run as root"
+    exit 1
+  fi
+}
+
+case "$1" in
+  start)
+    fail_unless_root
+    docker run -p 5000:5000 -v /etc/docker:/registry-conf -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml registry &
+    echo $! > $DOCKER_REGISTRY_PIDFILE
+    ;;
+
+  stop)
+    fail_unless_root
+    log_begin_msg "Stopping docker-regitry: $BASE"
+    start-stop-daemon --stop --pidfile "$DOCKER_REGISTRY_PIDFILE"
+    log_end_msg $?
+    ;;
+
+    *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
